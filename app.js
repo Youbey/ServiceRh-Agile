05ebb78c767c85144888631807a73e1ea41cbba8
Youbey <bendraou.e2100818@etud.univ-ubs.fr>
2025-04-29 17:41:44 +0200

    Login page & Logout button added and styled

--- app.js
+++ app.js
@@ -1,41 +1,34 @@
-var createError = require('http-errors');
+require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var session = require('express-session');
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var quizRouter = require('./routes/quiz');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// Middleware pour les sessions
+app.use(session({
+  secret: 'simpleLoginSecret',
+  resave: false,
+  saveUninitialized: true,
+}));
 
-app.use(logger('dev'));
+// Configuration des middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// Définir les routes
 app.use('/users', usersRouter);
+app.use('/quiz', quizRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// Gestion des erreurs 404
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', 'error.html'));
 });
 
 module.exports = app;
