73e9ee6e47212756b6bbe44ae57ec71d1206c426
Latour Morgan <latour.e2204863@etud.univ-ubs.fr>
2025-05-05 12:05:53 +0200

    change app.js

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-var createError = require('http-errors');
+require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var logger = require('morgan');
 
-const tokenRouter = require('./routes/gentoken.js');
+
+
+var tokenRouter = require('./routes/gentoken.js');
+var usersRouter = require('./routes/users');
+var quizRouter = require('./routes/quiz');
+
 
 const app = express();
 
@@ -14,7 +20,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// Servir les fichiers statiques depuis /public
+// Configuration des middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Servir les fichiers HTML statiques depuis /views
@@ -25,29 +34,27 @@ app.get('/gentoken', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'genToken.html'));
 });
 
+// Middleware pour les sessions
+app.use(session({
+  secret: 'simpleLoginSecret',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+
+
+
 // Monter le routeur pour l'API de génération de token
 app.use('/api/token', tokenRouter);
+// Définir les routes
+app.use('/users', usersRouter);
+app.use('/quiz', quizRouter);
 
-// Catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Gestion des erreurs 404
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', 'error.html'));
 });
 
-// Error handler
-app.use(function(err, req, res, next) {
-  // Set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // Render the error page (using a JSON response for simplicity)
-  res.status(err.status || 500);
-  res.json({ error: err.message });
-});
-
-// Démarrer le serveur
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
 
 module.exports = app;
