e79c3d416960c6715159a4de4535912ddf13350f
Latour Morgan <latour.e2204863@etud.univ-ubs.fr>
2025-05-05 12:26:32 +0200

    push app.js update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var session = require('express-session');
 
-
+var tokenRouter = require('./routes/gentoken.js');
 var usersRouter = require('./routes/users');
 var quizRouter = require('./routes/quiz');
 
@@ -32,8 +32,20 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Définir les routes
+app.use('/api/token', tokenRouter);
 app.use('/users', usersRouter);
 app.use('/quiz', quizRouter);
+app.use("/", indexRouter);
+
+
+// Servir les fichiers HTML statiques depuis /views
+app.use('/views', express.static(path.join(__dirname, 'views')));
+
+// Route spécifique pour servir genToken.html à /gentoken
+app.get('/gentoken', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'genToken.html'));
+});
+
 
 // Gestion des erreurs 404
 app.use((req, res, next) => {
